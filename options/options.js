f234f20a82935bc566442deca4d3730dd2dd5ae8
Crowdbotics <team@crowdbotics.com>
2022-08-06 19:28:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "photographer_calend_36638",
+  url: "https://photographer_calend_36638.botics.co",
+  api: "https://photographer_calend_36638.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
